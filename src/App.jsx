729748cb20d7cf310b0b8c40f3f23d28acaf324d
carinotj-19 <67515859+carinotj19@users.noreply.github.com>
2025-04-18 20:22:48 +0800

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom';
-import Quizpage from './pages/Quizpage';
-import Homepage from './pages/Homepage';
+import Quizpage from "./pages/Quizpage";
+import Homepage from "./pages/Homepage";
 import Loginpage from "./pages/Loginpage";
 import Registerpage from "./pages/Registerpage";
 import Historypage from "./pages/Historypage";
 import Leaderboardpage from "./pages/Leaderboardpage";
-
+import {
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 function App() {
   return (
     <Routes>
@@ -16,8 +18,10 @@ function App() {
       <Route path="/register" element={<Registerpage />} />
       <Route path="/history" element={<Historypage />} />
       <Route path="/leaderboard" element={<Leaderboardpage />} />
+
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
 
-export default App
+export default App;
