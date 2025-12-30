19012411f463a38d787a9a1f53fa5f5281f14642
carinotj-19 <67515859+carinotj19@users.noreply.github.com>
2025-12-30 10:16:44 +0800

    update url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/brainybox/" : "/",
+  base: mode === "production" ? "/BrainyBox/" : "/",
   plugins: [react(), tailwindcss()],
 }));
